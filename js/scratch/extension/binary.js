c5e178a6b7c4cd62699be61f580e0aa66a4cd9f2
Anomaly556 <73934950+Anomaly556@users.noreply.github.com>
2021-12-05 19:17:57 +0900

    Create binary.js

---
+++ js/scratch/extension/binary.js
@@ -0,0 +1,44 @@
+class binary {
+  getInfo() {
+    return {
+      id: "binaryEx",
+      name: "Binary Extension",
+      description: "Fun with a Binary!",
+      blocks: [
+        {
+          opcode: 'binToTxt',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'binary [BIN] to Text',
+          arguments: {
+            BIN: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '01000010 01101001 01101110 01100001 01110010 01111001'
+            }
+          }
+        },
+        {
+          opcode: 'txtToBin',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'text [TEXT] to binary',
+          arguments: {
+            TEXT: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'Binary'
+            }
+          }
+        }
+      ]
+    }
+  }
+  binToTxt(args) {
+    var bin = args.BIN.toString();
+    
+    return binary.split(" ").map((x) => x = String.fromCharCode(parseInt(x, 2))).join("");
+  }
+  txtToBin(args) {
+    var text = args.TEXT.toString();
+    
+    return Array.from(text).map((each)=>each.charCodeAt(0).toString(2)).join("")
+  }
+}
+Scratch.extensions.register(new binary());
