3fd8b3306a81199959c9f590c9f88b83cb64a75d
anything <73934950+NoteSwiper@users.noreply.github.com>
2022-12-08 18:46:33 +0900

    s

--- sw.js
+++ sw.js
@@ -1,36 +1,43 @@
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        (async function () {
-            const cacheNames = await caches.keys();
-            await Promise.all(
-                cacheNames.filter((cacheName) => {
-
-                }).map((cacheName) => caches.delete(cacheName)),
-            );
-        })(),
-    );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async function () {
+      const cacheNames = await caches.keys();
+      await Promise.all(
+        cacheNames
+          .filter((cacheName) => {})
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })()
+  );
 });
 
-self.addEventListener('fetch', function(e) {
-    event.respondWith(
-        async () => {
-            const cacheResponse = await caches.match(event.request);
-            if (cacheResponse) {
-                return cacheResponse;
-            }
+self.addEventListener("fetch", (e) => {
+  event.respondWith(async () => {
+    // キャッシュがあった場合は、キャッシュの内容を返す。
+    const cacheResponse = await caches.match(event.request);
+    if (cacheResponse) {
+      return cacheResponse;
+    }
 
-            const fetchRequest = event.request.clone();
-            const fetchReponse = await fetch(fetchRequest);
+    // request を複製する（ストリームは再利用できないので）
+    const fetchRequest = event.request.clone();
+    const fetchResponse = await fetch(fetchRequest);
 
-            if (!fetchReponse || fetchReponse.status !== 200 || fetchReponse.type !== 'basic') {
-                return fetchReponse;
-            }
+    // レスポンスが正しくない場合はそのまま返却
+    if (
+      !fetchResponse ||
+      fetchResponse.status !== 200 ||
+      fetchResponse.type !== "basic"
+    ) {
+      return fetchResponse;
+    }
 
-            const responseToCache = fetchResponse.clone();
-            const cache = await caches.open(CacheName);
+    // response を複製する（こちらも同じくストリームは再利用できないので）
+    const responseToCache = fetchResponse.clone();
+    const cache = await caches.open(CacheName);
 
-            cache.put(event.request, responseToCache);
-            return fetchReponse;
-        }
-    );
-})
+    // cache に登録する
+    cache.put(event.request, responseToCache);
+    return fetchResponse;
+  });
+});
