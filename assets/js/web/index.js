14318ede237e048874f36f1a13f573b5b9def716
anything <73934950+NoteSwiper@users.noreply.github.com>
2023-06-04 22:09:28 +0900

    s

--- assets/js/web/index.js
+++ assets/js/web/index.js
@@ -1,20 +1,44 @@
+/*
+https://noteswiper.github.io/?connected=hateblog&webname=plgr
+*/
+
 function getParam(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-if(getParam("usrname")) {
-	document.getElementById('welcome').innerHTML = `hello, ` + getParam("usrname") + "!";
+function faviconSetting() {
+  var item = $('a');
+  item.each(function () {
+    var faviconUrl = $(this).attr('href');
+    var url = "http://www.google.com/s2/favicons?domain=" + faviconUrl;
+    if (url) { $(this).prepend('<img src="' + url + '">') } else { $(this).prepend('<img src="https://noteswiper.github.io/assets/svg/notfound.svg">') }
+  });
 }
-if(getParam("lang")) {
+faviconSetting();
+
+var connected = getParam("connected");
+var webname = getParam("webname");
+var usrname = getParam("usrname");
+var lang = getParam("lang");
+
+
+
+if (getParam("connected")) {
+  
+}
+
+if (getParam("usrname")) {
+  document.getElementById('welcome').innerHTML = `hello, ` + getParam("usrname") + "!";
+}
+if (getParam("lang")) {
   let current = location.origin + location.pathname;
   let curparam = location.search;
   let curhash = location.hash;
   location.href = current + getParam("lang");
 }
-
